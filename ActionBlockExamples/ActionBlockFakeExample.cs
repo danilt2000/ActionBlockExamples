ef08b75c10395d757c43d9ece17075c339b32f48
Hepatica <danil.tkachenko@getmore.cz>
2024-06-11 23:54:39 +0200

    Add project files.

---
+++ ActionBlockExamples/ActionBlockFakeExample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace ActionBlockExamples
+{
+        internal class ActionBlockFakeExample
+        {
+                internal async void Example()
+                {
+                        var actionBlock = new ActionBlock<string>(async fileName =>
+                        {
+                                Console.WriteLine($"Начата обработка: {fileName}");
+                                await Task.Delay(1000);
+                                Console.WriteLine($"Завершена обработка: {fileName}");
+                        }, new ExecutionDataflowBlockOptions
+                        {
+                                MaxDegreeOfParallelism = 2 // Processing 2 entity in parallel 
+                        });
+
+                        string[] imageFiles = { "image1.jpg", "image2.png", "image3.bmp", "image4.gif" };
+
+                        foreach (var file in imageFiles)
+                        {
+                                actionBlock.Post(file);
+                        }
+
+                        actionBlock.Complete();
+                        await actionBlock.Completion;
+                }
+        }
+}
---
+++ ActionBlockExamples/ActionBlockHttpExample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace ActionBlockExamples
+{
+        internal class ActionBlockHttpExample
+        {
+                internal async void Example()
+                {
+
+                        var actionBlock = new ActionBlock<string>(async url =>
+                        {
+                                using (HttpClient client = new HttpClient())
+                                {
+                                        Console.WriteLine($"Начало скачивания: {url}");
+                                        var data = await client.GetStringAsync(url);
+                                        Console.WriteLine($"Завершено скачивание: {url}, длина данных: {data.Length}");
+                                }
+                        }, new ExecutionDataflowBlockOptions
+                        {
+                                MaxDegreeOfParallelism = 3 // Download 3 entity in parallel 
+                        });
+
+                        string[] urls =
+                        {
+                                "https://example.com/file1",
+                                "https://example.com/file2",
+                                "https://example.com/file3",
+                                "https://example.com/file4"
+                        };
+
+                        foreach (var url in urls)
+                        {
+                                actionBlock.Post(url);
+                        }
+
+                        actionBlock.Complete();
+                        await actionBlock.Completion;
+
+                }
+
+        }
+}
